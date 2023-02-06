9c1c7edffafb215f09eb7ca40000ceb0e8a4416d
KELSEY-LAPTOP\Kelsey <k.whitlock@hotmail.ca>
2023-02-06 12:53:50 -0700

    figuring out http home session

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -11,21 +11,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Kelsey
  */
 public class HomeServlet extends HttpServlet {
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
-
-    
+  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+         HttpSession session = request.getSession();
        
     }
 
@@ -33,7 +30,7 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
     }
 
   
